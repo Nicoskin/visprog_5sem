90765d7b77834b319cd733c1c50ffc8b21093c37
Пушница Владимир Алексеевич <ia131s20@csc.sibsutis.ru>
2023-10-02 19:49:29 +0700

    сделал стенки чуть получше

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,9 +17,9 @@ inline bool intersect_1 (int a, int b, int c, int d) {
 }
 
 bool intersect (pt a, pt b, pt c, pt d) {
-    return /*intersect_1 (a.x, b.x, c.x, d.x)
+    return intersect_1 (a.x, b.x, c.x, d.x)
         && intersect_1 (a.y, b.y, c.y, d.y)
-        &&*/ area(a,b,c) * area(a,b,d) <= 0
+        && area(a,b,c) * area(a,b,d) <= 0
         && area(c,d,a) * area(c,d,b) <= 0;
 }
 
@@ -37,21 +37,15 @@ MainWindow::MainWindow(QWidget *parent)
         int min_dbm = -60;
         int max_dbm = -50;
 
-        float scale = 2 ; //1 пискель = 1/scale метров
+        float scale = 0.1; //1 пискель = 1/scale метров
 
         QPainter p(&map);
 
         //стенка
-        pt a;
-        a.x = 500;
-        a.y = 300;
-        pt b;
-        b.x = 550;
-        b.y = 400;
+        pt A{500,300};
+        pt B{550,400};
 
-        pt c;
-        c.x = wifi_x;
-        c.y = wifi_y;
+        pt D{wifi_x,wifi_y};
 
 
         //поиск минимума
@@ -60,32 +54,44 @@ MainWindow::MainWindow(QWidget *parent)
         float dBm = ant + tx - PL;
         max_dbm = dBm;
 
-        for(int i = 0; i < 1000; i++){
-            for(int j = 0; j < 1000; j++){
-                d = sqrt(pow((wifi_x-i),2)+pow((wifi_y-j),2))/scale;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                d = sqrt(pow((wifi_x - i), 2) + pow((wifi_y - j), 2)) / scale; // Расстояние между точкой (i, j) и точкой (wifi_x, wifi_y)
 
-                pt D;
-                D.x = i;
-                D.y = j;
+                pt C{i, j}; // Создаем точку D с координатами (i, j)
 
-                if (intersect(a,b,c,D) != 0) d = d + 80;//+80 дальность за стенкой
+                // Проверяем пересечение с стеной
+                bool wallIntersection = intersect(A, B, C, D);
 
-                PL = 28 + 22*log10(d) + 20*log10(fc);
-                dBm = ant + tx - PL;
-                if (d == 0) dBm = 10;
+                // Рассчитываем Path Loss (PL) и dBm
+                PL = 28 + 22 * log10(d) + 20 * log10(fc); // Рассчет потерь сигнала (Path Loss)
+                dBm = ant + tx - PL; // Рассчет уровня сигнала в децибеллах (dBm)
 
-                if (dBm < min_dbm) min_dbm = dBm;
+                // Исключаем точки, которые лежат на линии стены (A, B)
+                if ((wallIntersection && !((D.x == A.x && D.y == A.y) || (D.x == B.x && D.y == B.y)))) {
+                    p.setPen(QColor(0, 0, 0, 255)); // Устанавливаем прозрачный цвет для этих точек
+                    //cout << "yes";
+                } else {
+                    // Корректируем dBm, когда нет пересечения со стеной
+                    if (d == 0) {
+                        dBm = -10; // Здесь можно корректировать значение -10 по необходимости
+                    }
 
-                dBm = ((dBm * -1) -8) * 3.86; // коэффициент можно менять 1.875=-144blue
+                    // Проверяем dBm по отношению к минимальному значению (min_dbm)
+                    if (dBm < min_dbm) min_dbm = dBm;
 
-                if(dBm < 0) p.setPen(QColor(255,0,0, 255));
-                else if(dBm < 64 ) p.setPen(QColor(255, dBm*4, 0, 255));
-                else if(dBm < 128) p.setPen(QColor((255 - ((dBm-64)*4)), 255, 0, 255));
-                else if(dBm < 192) p.setPen(QColor(0, 255, ((dBm-128)*4), 255));
-                else if(dBm < 256) p.setPen(QColor(0, 255 - ((dBm-192)*4), 255, 255));
-                else p.setPen(QColor(0,0,200, 255));
+                    // Рассчитываем цвет на основе dBm
+                    dBm = ((dBm * -1) - 44) * 2.55;
 
-                p.drawPoint(i, j);
+                    if (dBm < 0) p.setPen(QColor(255, 0, 0, 255));
+                    else if (dBm < 64) p.setPen(QColor(255, dBm * 4, 0, 255));
+                    else if (dBm < 128) p.setPen(QColor((255 - ((dBm - 64) * 4)), 255, 0, 255));
+                    else if (dBm < 192) p.setPen(QColor(0, 255, ((dBm - 128) * 4), 255));
+                    else if (dBm < 256) p.setPen(QColor(0, 255 - ((dBm - 192) * 4), 255, 255));
+                    else p.setPen(QColor(0, 0, 200, 255));
+
+                    p.drawPoint(i, j);
+                }
             }
         }
         float k = (min_dbm - max_dbm) * -1;
@@ -93,7 +99,7 @@ MainWindow::MainWindow(QWidget *parent)
         std::cout << " max=" << max_dbm<< "    разница=" << k << "    коэффициент=" << 255/k << "\n";
         p.end();
         scene->addPixmap(map);
-        scene->addLine(a.x, a.y, b.x, b.y);
+        //scene->addLine(A.x, A.y, B.x, B.y);
         QGraphicsView* view = new QGraphicsView(scene);
         setCentralWidget(view);
 }
